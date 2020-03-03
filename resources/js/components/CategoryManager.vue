915bc20bb1272c78f80479ec7c94e6cae1133e72
Anders Eriksson <digitaldoktorn@gmail.com>
2020-03-03 22:47:52 +0100

    lesson 2.4 Injecting props in Vue components

---
+++ resources/js/components/CategoryManager.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-for="category in categories" :key="category.id">
+            {{ category.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CategoryManager",
+        props: ['categories']
+    }
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 //Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+
+Route::get('/categories', 'CategoryController@index');
