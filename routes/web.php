b6ae7eed3f5574a965f048ad82820b3170e065e3
Anders Eriksson <digitaldoktorn@gmail.com>
2020-03-03 22:21:42 +0100

    lesson 2.3

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 //Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 //Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
