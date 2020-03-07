84437007423a8ef8fcdebdd655f1e61d4200e3b4
Anders Eriksson <digitaldoktorn@gmail.com>
2020-03-07 11:50:55 +0100

    Lesson 2.7 Protecting routes and models with policies

---
+++ app/Policies/CategoryPolicy.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Policies;
+
+use App\Category;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CategoryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any categories.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    public function before($user, $ability){
+    }
+
+    /**
+     * Determine whether the user can manage categories.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function manage(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create categories.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the category.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function update(User $user, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the category.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function delete(User $user, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the category.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function restore(User $user, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the category.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Category  $category
+     * @return mixed
+     */
+    public function forceDelete(User $user, Category $category)
+    {
+        //
+    }
+}
